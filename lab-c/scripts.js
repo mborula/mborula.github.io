6dfd7614797b149df5dda8243f1a8b408e3772dd
Borula Mateusz <bm57939>
2026-04-12 13:51:39 +0200

    Lab3

--- lab-c/scripts.js
+++ lab-c/scripts.js
@@ -1,5 +1,5 @@
 const x = document.getElementById("demo");
-
+let mapa;
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
@@ -15,14 +15,43 @@ function success(position) {
 }
 
 function error() {
+  alert("Couldn't get your location, showing default location");
   getMap([53.447084, 14.49183])
 }
 
 
 function getMap(position) {
-  const map = L.map('map').setView(position, 70);
+  mapa = L.map('map').setView(position, 70);
   L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: '&copy; <a href="https://www.esri.com/">Esri</a> &amp; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-  L.marker(position).addTo(map)
+  }).addTo(mapa);
+  L.marker(position).addTo(mapa)
+}
+
+function saveRasterMap() {
+  if (mapa) {
+    leafletImage(mapa, function(err, canvas) {
+      if (err) {
+        console.log('Błąd podczas generowania obrazu:', err);
+        return;
+      }
+
+      const imgUrl = canvas.toDataURL();
+      const link = document.createElement('a');
+      link.href = imgUrl;
+      link.download = 'mapa.png';
+      link.click();
+    });
+  } else {
+    console.log("Mapa nie została załadowana!");
+  }
+}
+
+
+
+class Tile {
+  constructor(id) {
+    this.id = id;
+  }
+
 }
