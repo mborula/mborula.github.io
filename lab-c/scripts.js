2ea10e4b9ca20ed0821c8951b17ca7871257c714
Borula Mateusz <bm57939>
2026-04-17 11:43:59 +0200

    Lab3 update

--- lab-c/scripts.js
+++ lab-c/scripts.js
@@ -1,5 +1,20 @@
+class Tile {
+  constructor(id, canvas) {
+    this.id = id;
+    this.canvas = canvas;
+  }
+}
+
+class Table {
+  constructor() {
+    this.tiles = [];
+  }
+}
+
 const x = document.getElementById("demo");
 let mapa;
+const tiles = []
+const table = new Table();
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
@@ -25,33 +40,64 @@ function getMap(position) {
   L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: '&copy; <a href="https://www.esri.com/">Esri</a> &amp; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(mapa);
-  L.marker(position).addTo(mapa)
 }
 
-function saveRasterMap() {
-  if (mapa) {
-    leafletImage(mapa, function(err, canvas) {
-      if (err) {
-        console.log('Błąd podczas generowania obrazu:', err);
-        return;
-      }
+function getMapImage() {
+  if (!mapa) return;
 
-      const imgUrl = canvas.toDataURL();
-      const link = document.createElement('a');
-      link.href = imgUrl;
-      link.download = 'mapa.png';
-      link.click();
-    });
-  } else {
-    console.log("Mapa nie została załadowana!");
+  leafletImage(mapa, function(err, canvas) {
+    if (err) {
+      console.log('Błąd podczas generowania obrazu:', err);
+      return;
+    }
+    tiles.length = 0; // czyszczenie starych tiles
+    divideToTiles(canvas)
+    for (let i = tiles.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
+    }
+    displayTiles()
+  });
+}
+
+function divideToTiles(canvas) {
+  const tileWidth = canvas.width / 4;
+  const tileHeight = canvas.height / 4;
+
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 4; x++) {
+      const tileCanvas = document.createElement('canvas');
+      tileCanvas.width = tileWidth;
+      tileCanvas.height = tileHeight;
+      const ctx = tileCanvas.getContext('2d');
+      ctx.drawImage(canvas,x * tileWidth, y * tileHeight, tileWidth, tileHeight,0, 0, tileWidth, tileHeight);
+
+      tile = new Tile(`{x}{y}`, tileCanvas)
+
+      tiles.push(tile);
+
+      const emptyCanvas = document.createElement('canvas');
+      emptyCanvas.width = tileWidth;
+      emptyCanvas.height = tileHeight;
+      table.tiles.push(new Tile(`{x}{y}`, emptyCanvas));
+    }
   }
 }
 
 
+function displayTiles() {
+  const container = document.querySelector('.container-puzzle');
+  container.innerHTML = '';
+  tiles.forEach(tile => {
+    const img = document.createElement('img');
+    img.src = tile.canvas.toDataURL();
+    container.appendChild(img);
 
-class Tile {
-  constructor(id) {
-    this.id = id;
-  }
-
+  });
 }
+
+
+
+
+
+
